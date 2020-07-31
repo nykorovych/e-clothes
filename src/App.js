225b7505bd378fe54860026017d0a019307f5c57
Iurii <ny.iuriii@gmail.com>
2020-07-31 18:12:49 +0200

    before cart

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { setCurrentUser } from "./redux/user/user.actions";
 
@@ -41,13 +41,26 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SigninAndSignUpPage} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to="/"></Redirect>
+              ) : (
+                <SigninAndSignUpPage></SigninAndSignUpPage>
+              )
+            }
+          />
         </Switch>
       </div>
     );
   }
 }
+const mstp = ({ user }) => ({
+  currentUser: user.currentUser,
+});
 const mdtp = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
-export default connect(null, mdtp)(App);
+export default connect(mstp, mdtp)(App);
