8b3e33241a5b0cd0d309ce6e30b3d74301c3cae3
Iurii <ny.iuriii@gmail.com>
2020-08-02 14:04:44 +0200

    added arrows with increasing adn decrising funciaonality

--- src/redux/cart/cart-action.js
+++ src/redux/cart/cart-action.js
@@ -6,10 +6,20 @@ export const toggleAction = () => {
   };
 };
 
-
-export const addItem = item => {
+export const addItem = (item) => {
   return {
     type: CartActionType.ADD_ITEM,
-    payload: item
-  }
-}
+    payload: item,
+  };
+};
+export const removeItem = (item) => {
+  return {
+    type: CartActionType.REMOVE_ITEM,
+    payload: item,
+  };
+};
+
+export const clearItemFromCart = (item) => ({
+  type: CartActionType.CLEAR_ITEM_FROM_CART,
+  payload: item,
+});
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,17 +1,30 @@
-
-
 export const addItemToCart = (cartItems, cartItemToAdd) => {
-    const existingCartItem = cartItems.find(
-      cartItem => cartItem.id === cartItemToAdd.id
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToAdd.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
     );
-  
-    if (existingCartItem) {
-      return cartItems.map(cartItem =>
-        cartItem.id === cartItemToAdd.id
-          ? { ...cartItem, quantity: cartItem.quantity + 1 }
-          : cartItem
-      );
+  }
+
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
+
+export const removeItemFromCart = (cartItems, itemToRemove) => {
+  const existingItem = cartItems.find((item) => item.id === itemToRemove.id);
+
+  if (existingItem.quantity === 1) {
+    return cartItems.filter((item) => item.id !== itemToRemove.id);
+  }
+  return cartItems.map((item) => {
+    if (item.id === itemToRemove.id) {
+      return { ...item, quantity: item.quantity - 1 };
+    } else {
+      return item;
     }
-  
-    return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
-  };
+  });
+};
